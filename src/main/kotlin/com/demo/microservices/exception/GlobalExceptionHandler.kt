bc9bc53cb4bd4387378e55fcbaeefd2ea610d4b0
ViniciusTechnology <devopsengineer048@gmail.com>
2026-04-05 18:22:32 -0300

    GlobalExceptionHandler

---
+++ src/main/kotlin/com/demo/microservices/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,23 @@
+package com.demo.microservices.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleValidationExceptions(ex: MethodArgumentNotValidException): ResponseEntity<Map<String, String?>> {
+        val errors = ex.bindingResult.fieldErrors.associate { it.field to it.defaultMessage }
+        return ResponseEntity(errors, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(RuntimeException::class)
+    fun handleRuntimeException(ex: RuntimeException): ResponseEntity<Map<String, String?>> {
+        val error = mapOf("error" to ex.message)
+        return ResponseEntity(error, HttpStatus.NOT_FOUND)
+    }
+}
